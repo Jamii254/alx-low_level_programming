d360b3d1f6d444d06f685b6ee141dd2e741993f3
my <me@gmail.com>
2023-03-23 23:44:28 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,18 @@
 #include "main.h"
+
 /**
- * _isdigit - checks if parameter is a number between 0 o 9.
+ * _isdigit - checks if parameter is a number between 0 to 9.
  * @c: input number.
- * Return: 1 if a number (0 to 9), 0 in other case.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
  */
-int_isdigit(int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
